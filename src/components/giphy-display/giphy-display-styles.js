6e665dfe1f2fa08c635f27316fb0f4c303536985
SPDUK <spdevuk@gmail.com>
2019-06-24 01:36:43 +0100

    feat: add copy to clipboard button and notification

--- src/components/giphy-display/giphy-display-styles.js
+++ src/components/giphy-display/giphy-display-styles.js
@@ -1,8 +1,18 @@
 import styled from 'styled-components';
 
 export const CenteredImage = styled.img`
-  margin: 0 auto;
+  margin: 50px auto 15px auto;
   display: flex;
   justify-content: center;
   max-width: 100%;
+  @media (min-width: 500px) {
+    margin: 15px auto;
+  }
+`;
+
+export const StyledActions = styled.div`
+  display: flex;
+  justify-content: space-between;
+  max-width: 100%;
+  padding-bottom: 15px;
 `;
--- src/components/giphy-display/giphy-display.jsx
+++ src/components/giphy-display/giphy-display.jsx
@@ -1,17 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import ModalDialog, { ModalTransition } from '@atlaskit/modal-dialog';
-import { CenteredImage } from './giphy-display-styles';
+import Button from '@atlaskit/button';
+import { ToastContainer, toast } from 'react-toastify';
+import { CenteredImage, StyledActions } from './giphy-display-styles';
+import 'react-toastify/dist/ReactToastify.css';
+
+const copyToClipBoard = async giphy => {
+  try {
+    await navigator.clipboard.writeText(giphy);
+    toast('🦄 Copied URL to clipboard!');
+  } catch (e) {
+    toast.error("😡 Couldn't copy to clipboard!");
+  }
+};
 
 const giphyDisplay = ({ giphyIsOpen, giphyOnClose, giphy }) => (
-  <ModalTransition>
-    {giphyIsOpen && (
-      <ModalDialog onClose={giphyOnClose}>
-        <CenteredImage src={giphy} />
-        <div>{giphy}</div>
-      </ModalDialog>
-    )}
-  </ModalTransition>
+  <>
+    <ToastContainer position="top-right" autoClose={5000} draggable pauseOnHover={false} />
+    <ModalTransition>
+      {giphyIsOpen && (
+        <ModalDialog onClose={giphyOnClose}>
+          <CenteredImage src={giphy} />
+          <StyledActions>
+            <Button aappearance="default" onClick={giphyOnClose}>
+              Close
+            </Button>
+
+            <Button onClick={() => copyToClipBoard(giphy)} autoFocus appearance="primary">
+              Copy to Clipboard
+            </Button>
+          </StyledActions>
+        </ModalDialog>
+      )}
+    </ModalTransition>
+  </>
 );
 
 giphyDisplay.propTypes = {
